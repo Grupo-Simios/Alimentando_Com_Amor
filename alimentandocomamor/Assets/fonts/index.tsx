f6bda9a8893a79f3ac4522c19367cf63603642a5
Davi Santanna <davicalvalcanti@gmail.com>
2024-03-13 18:12:45 -0300

    Add file tsx for fonts

---
+++ alimentandocomamor/Assets/fonts/index.tsx
@@ -0,0 +1,13 @@
+import {Zilla_Slab, Outfit} from 'next/font/google';
+
+
+export const ZillaFont = Zilla_Slab({
+    weight: '300',
+    subsets: ['latin']
+})
+
+
+export const outfitFont = Outfit({
+    weight: '300',
+    subsets: ['latin']
+})
