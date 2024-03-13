448cc65721df500c648be3182d9f8cd9440b950f
Davi Santanna <davicalvalcanti@gmail.com>
2024-03-13 18:14:51 -0300

    add page rendered

---
+++ alimentandocomamor/src/app/page.tsx
@@ -0,0 +1,12 @@
+import {Footer} from '../../Assets/components/Footer'
+import "./globals.css";
+
+
+
+export default function Home() {
+  return (
+    <main className='h-full'>
+      <Footer />
+    </main>
+  );
+}
