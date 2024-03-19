0295b6e6437febe60bb601c56e62e0f1cb839c5b
Davi Santanna <davicalvalcanti@gmail.com>
2024-03-19 17:52:11 -0300

    add new weight for fonts

--- Assets/fonts/index.tsx
+++ Assets/fonts/index.tsx
@@ -2,12 +2,12 @@ import {Zilla_Slab, Outfit} from 'next/font/google';
 
 
 export const ZillaFont = Zilla_Slab({
-    weight: '300',
+    weight:["300", "400", "500", "600", "700",],
     subsets: ['latin']
 })
 
 
 export const outfitFont = Outfit({
-    weight: '300',
+    weight: ["100","200","300", "400", "500", "600", "700","800","900"],
     subsets: ['latin']
 })
