395b103998bbf4422daff2948322c4c020c3889b
dinhoSilwa <dinhosilwa_@hotmail.com>
2024-04-23 00:07:33 -0300

    FIX: Added manipulations on global object data

--- src/components/footer.collection/footer/mobile/index.tsx
+++ src/components/footer.collection/footer/mobile/index.tsx
@@ -3,6 +3,7 @@ import { MdOutlineEmail } from "react-icons/md";
 import { ZillaFont, outfitFont } from "@/assets/fonts/index";
 import Link from "next/link";
 import clsx from "clsx";
+import { contactDetails } from "@/models/ongdetails";
 
 export const FooterMobile = () => {
 
@@ -53,14 +54,14 @@ const footerText = clsx(
           siga-nos nas redes
         </h2>
         <div className="flex h-fit gap-6 items-center justify-center">
-          <a href="#">
+          <a href="https://facebook.com/cyda.lima.35" target="_blanck">
             <FaFacebook
               color="#f5f5f5"
               className="text-5xl max-[195px]:text-2xl max-[285px]:text-4xl"
             />
           </a>
 
-          <a href="#">
+          <a href={"https://www.instagram.com/alimentandocomamornatal/"} target="_blanck" >
             <FaInstagram
               color="#f5f5f5"
               className="text-5xl max-[195px]:text-2xl max-[285px]:text-4xl"
@@ -104,8 +105,9 @@ const footerText = clsx(
               size={"20px"}
               className="max-[320px]:hidden"
             />
-            Número de telefone: 84 9873-6833
+           <a href={contactDetails.linkWhatsapp1}> Número de telefone: {contactDetails.tel1}</a>
           </p>
+      
 
           <p
             className={`${outfitFont.className} ${footerText} font-normal  leading-4  text-[#f5f5f5] text-xs rounded-lg flex gap-3 items-center   
@@ -130,7 +132,7 @@ const footerText = clsx(
 
             E-mail:
             <br />
-            voluntário@alimentandocomamornatal.org
+            {contactDetails.volunteerEmail}
           </p>
         </div>
       </div>
--- src/models/ongdetails.ts
+++ src/models/ongdetails.ts
@@ -1,7 +1,7 @@
-interface BankDetails {
-  ongName :string;
+export interface BankDetails {
+  ongName: string;
   CNPJ: string;
-  key:string;
+  key: string;
   qrCode: string;
   agency: string;
   operator: string;
@@ -9,49 +9,55 @@ interface BankDetails {
   bankName: string;
 }
 
-interface ContactDetails {
+export interface ContactDetails {
   tel1: string;
   tel2: string;
   volunteerEmail: string;
+  linkWhatsapp1: string;
+  linkWhatsapp2: string;
 }
 
-interface AddressDetails {
+export interface AddressDetails {
   mainAddress: string;
 }
 
-interface OngDetails {
+export interface SocialAddress {
+  instagram: string;
+  facebook: string;
+}
+
+export interface OngDetails {
   bankDetails?: BankDetails;
   contactDetails?: ContactDetails;
   addressDetails?: AddressDetails;
+  socialAddress?: SocialAddress;
 }
 
-export const ongDetails: OngDetails[] = [
-  {
-    bankDetails: {
-      ongName : "ASSOCIAÇÃO ALIMENTANDO COM AMOR",
-      CNPJ: "51.753.476/0001-34",
-      key: "00020126360014br.gov.bcb.pix011451753476000134 5204000053039865802BR5910----------6005 NATAL 62070503***630476E4",
-      qrCode: '',
-      agency: "0035",
-      operator: "003",
-      account: "10381-6",
-      bankName: "CAIXA ECONÔMICA FEDERAL"
-    }
-  },
-  {
-    contactDetails: {
-      tel1: '(84)998713-6833',
-      tel2: '(84)998169-0141',
-      volunteerEmail: 'voluntario@alimentandocomamornatal.org'
-    }
-  },
-  {
-    addressDetails: {
-      mainAddress: 'rua Santo Antônio 731 Cidade Alta, Natal RN'
-    }
-  }
-];
+export const bankDetails: BankDetails = {
+  ongName: "ASSOCIAÇÃO ALIMENTANDO COM AMOR",
+  CNPJ: "51.753.476/0001-34",
+  key: "00020126360014br.gov.bcb.pix011451753476000134 5204000053039865802BR5910----------6005 NATAL 62070503***630476E4",
+  qrCode: '',
+  agency: "0035",
+  operator: "003",
+  account: "10381-6",
+  bankName: "CAIXA ECONÔMICA FEDERAL"
+};
 
-export const externaldetailsbank = ongDetails[0].bankDetails
+export const contactDetails: ContactDetails = {
+  tel1: '(84)998713-6833',
+  tel2: '(84)998169-0141',
+  volunteerEmail: 'voluntario@alimentandocomamornatal.org',
+  linkWhatsapp1: "https://wa.me/55849987136833",
+  linkWhatsapp2:"https://wa.me/55849981690141"
 
+};
 
+export const addressDetails: AddressDetails = {
+  mainAddress: 'rua Santo Antônio 731 Cidade Alta, Natal RN'
+};
+
+export const socialAddress: SocialAddress = {
+  instagram: "https://www.instagram.com/alimentandocomamornatal/",
+  facebook: "https://facebook.com/cyda.lima.35"
+};
