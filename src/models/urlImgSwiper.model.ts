76bf41c410215166cec440d845529a5a4c29e84a
Davi Santanna <davicalvalcanti@gmail.com>
2024-04-30 20:39:50 -0300

    FEAT: "add a new props of array  simiosTeam"

--- src/models/urlImgSwiper.model.ts
+++ src/models/urlImgSwiper.model.ts
@@ -1,23 +1,135 @@
-const imgDash = "https://i.imgur.com/"
-
+const imgDash = "https://i.imgur.com/";
 
 export const urlImgdata = [
-  { id: "1", Image: "https://i.imgur.com/LUbQcEG.jpg", altImg : "a imagem mostra : a organizadora da ong sendo enttrevistada" },
-  { id: "2", Image: "https://i.imgur.com/vF40iZT.jpg", altImg : "a imagem mostra : Beneficiários na entrada da ong" },
-  { id: "3", Image: "https://i.imgur.com/34AHrZe.jpg.png", altImg : "a imagem mostra : Beneficiários Na parte externa da ong " },
-  { id: "4", Image: "https://i.imgur.com/fPsN33X.jpg.png", altImg : "a imagem mostra : Um panorama de Todos que ajudam e que são beneficiados " },
-  { id: "4", Image: "https://i.imgur.com/mFhNTyG.jpg.png", altImg : "a imagem mostra : A distribuição de Alimentos pela ong" },
+  {
+    id: "1",
+    Image: "https://i.imgur.com/LUbQcEG.jpg",
+    altImg: "a imagem mostra : a organizadora da ong sendo entrevistada",
+  },
+  {
+    id: "2",
+    Image: "https://i.imgur.com/vF40iZT.jpg",
+    altImg: "a imagem mostra : Beneficiários na entrada da ong",
+  },
+  {
+    id: "3",
+    Image: "https://i.imgur.com/34AHrZe.jpg.png",
+    altImg: "a imagem mostra: Beneficiários Na parte externa da ong ",
+  },
+  {
+    id: "4",
+    Image: "https://i.imgur.com/fPsN33X.jpg.png",
+    altImg:
+      "a imagem mostra : Um panorama de Todos que ajudam e que são beneficiados ",
+  },
+  {
+    id: "4",
+    Image: "https://i.imgur.com/mFhNTyG.jpg.png",
+    altImg: "a imagem mostra : A distribuição de Alimentos pela ong",
+  },
 ];
 
 export const simiosTeam = [
-  {id : 'selenium', image : "QZG6CsK.png", altImg: "Foto de selenium"},
-  {id : 'tester', image : "https://i.imgur.com/Nt01rad.png", altImg: "Foto do tester"},
-  {id : 'erick', image : "https://i.imgur.com/4FpMoHw.png", altImg: "Foto do erick"},
-  {id : 'ana', image : "https://i.imgur.com/OOAn04y.png", altImg: "Foto do ana"},
-  {id : 'Dinho', image : "https://i.imgur.com/1zSU8Dd.png", altImg: "Foto do Dinho"},
-  {id : 'italo', image : "https://i.imgur.com/RWeDj0C.png", altImg: "Foto do italo"},
-  {id : 'davi', image : "https://i.imgur.com/6DBmqZt.png", altImg: "Foto do davi"},
-  {id : 'documenter', image : "https://i.imgur.com/4LjUIUC.png", altImg: "Foto do documenter"},
-  {id : 'back', image : "https://i.imgur.com/gSFj3OM.png", altImg: "Foto do back"},
+  {
+    id: "tester",
+    key: 1,
+    image: "https://i.imgur.com/Nt01rad.png",
+    name: "Luis Carlos Duarte",
+    github: "",
+    linkedin:
+      "https://www.linkedin.com/in/luiscduarte?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+    mainFunction: "Tester",
+    adm: false,
+  },
 
-]
+  {
+    id: "erick",
+    key: 2,
+    image: "https://i.imgur.com/4FpMoHw.png",
+    name: "Erick Rosa",
+    github: "https://github.com/Erickhrs",
+    linkedin:
+      "https://www.linkedin.com/in/erick-rosa-12b83a228?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+    mainFunction: "",
+    adm: true,
+  },
+
+  {
+    id: "ana",
+    key: 3,
+    image: "https://i.imgur.com/OOAn04y.png",
+    name: "Ana Martins",
+    github: "",
+    linkedin:
+      "https://www.linkedin.com/in/ana-martins-129657261?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+    mainFunction: "",
+    adm: true,
+  },
+
+  {
+    id: "Dinho",
+    key: 4,
+    image: "https://i.imgur.com/1zSU8Dd.png",
+    name: "Cláudio Silva",
+    github: "https://github.com/dinhoSilwa",
+    linkedin: "",
+    mainFunction: "Desenvolvedor web",
+    adm: false,
+  },
+
+  {
+    id: "icaro",
+    key: 5,
+    image: "https://i.imgur.com/RWeDj0C.png",
+    name: "Icaro Filipe",
+    github: "https://github.com/IcaroFilipeD2",
+    linkedin: "",
+    mainFunction: "Analista de Sistemas",
+    adm: false,
+  },
+
+  {
+    id: "davi",
+    key: 6,
+    image: "https://i.imgur.com/6DBmqZt.png",
+    name: "Davi Santanna",
+    github: "https://github.com/DaviSC17",
+    linkedin: "",
+    mainFunction: "Desenvolvedor web",
+    adm: false,
+  },
+
+  {
+    id: "documenter",
+    key: 7,
+    image: "https://i.imgur.com/4LjUIUC.png",
+    name: "Gabriel Santiago",
+    github: "https://github.com/gabriel-sant123",
+    linkedin:
+      "https://www.linkedin.com/in/gabriel-santiago-a2112b22a?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+    mainFunction: "Documenter",
+    adm: false,
+  },
+
+  {
+    id: "back",
+    key: 8,
+    image: "https://i.imgur.com/gSFj3OM.png",
+    name: "João Gabriel",
+    github: "https://github.com/JoaoGabsSR",
+    linkedin: "www.linkedin.com/in/joão-gabriel-34a70519a",
+    mainFunction: "Desenvolvedor",
+    adm: false,
+  },
+
+  {
+    id: "designer",
+    key: 9,
+    image: "https://i.imgur.com/Nt01rad.png",
+    name: "Gabriel Ferreira",
+    github: "",
+    linkedin: "",
+    mainFunction: "UX/UI designer",
+    adm: false,
+  },
+];
