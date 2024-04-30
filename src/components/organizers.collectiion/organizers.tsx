6c6033dd94927a8a908ef146ff7703e0b34eaa7c
Davi Santanna <davicalvalcanti@gmail.com>
2024-04-30 20:40:48 -0300

    FEAT: "add a new props to use  with component Members"

--- src/components/organizers.collectiion/organizers.tsx
+++ src/components/organizers.collectiion/organizers.tsx
@@ -1,7 +1,10 @@
 import { ZillaFont } from "@/assets/fonts";
+import Link from "next/link";
 import { FaGithub, FaLinkedin } from "react-icons/fa";
 
 interface profile {
+  hasGithub: boolean;
+  hasLinkedin: boolean;
   linkedin: string;
   github: string;
   name: string;
@@ -14,47 +17,50 @@ export const Members = (profile: profile) => {
     <div className="flex flex-col w-fit gap-5  justify-center items-center">
       <figure className="max-w-52 max-[310px]:px-4">
         <img
+          width={190}
           className="rounded"
           src={profile.image}
           alt={`Imagem de ${profile.name}`}
         />
       </figure>
-      <div className="flex flex-col  gap-5">
-        <span
-          className={`${ZillaFont.className} font-normal text-lg leading-7 text-center text-[#1b1b1bcc]
+      <div className="flex flex-col mb-10 gap-3">
+        <h3
+          className={`${ZillaFont.className} font-semibold antialised text-lg leading-7 text-center text-[#2e2c2ccc]
           
 
           max-[190px]:text-sm
-          max-[310px]:text-lg
-
-          max-[325px]:text-xs
-          max-[360px]:text-sm
+          max-[310px]:text-sm
           max-[400px]:text-base
           
           `}
         >
           {profile.name},
           <br /> {profile.mainFunction}
-        </span>
-        <div className="gap-3 flex self-center">
-          <FaGithub
-            cursor={"pointer"}
-            href={profile.github}
-            className="text-3xl
+        </h3>
+        <div className="gap-6 flex self-center">
+          {profile.hasGithub ? (
+            <Link href={profile.github} target="_blank">
+              <FaGithub
+                title={`Ir para o Github do ${profile.name}`}
+                cursor={"pointer"}
+                className="text-4xl max-[190px]:text-xl max-[320px]:text-2xl"
+              />
+            </Link>
+          ) : (
+            <></>
+          )}
 
-          max-[190px]:text-xl
-          max-[320px]:text-2xl
-          "
-          />
-          <FaLinkedin
-            cursor={"pointer"}
-            href={profile.linkedin}
-            className="text-3xl 
-            
-          max-[190px]:text-xl
-          max-[320px]:text-2xl
-          "
-          />
+          {profile.hasLinkedin ? (
+            <Link href={profile.linkedin} target="_blank">
+              <FaLinkedin
+                title={`Ir para o Linkedin do ${profile.name}`}
+                cursor={"pointer"}
+                className="text-4xl max-[190px]:text-xl max-[320px]:text-2xl"
+              />
+            </Link>
+          ) : (
+            <></>
+          )}
         </div>
       </div>
     </div>
