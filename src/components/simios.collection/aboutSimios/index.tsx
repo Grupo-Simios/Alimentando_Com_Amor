80f9bebeb1e7d3a33055de6300059172ee18c450
Davi Santanna <davicalvalcanti@gmail.com>
2024-05-03 19:40:05 -0300

    STYLE: "change style in Container and delete paragraphAboutSimios clsx"

--- src/components/simios.collection/aboutSimios/index.tsx
+++ src/components/simios.collection/aboutSimios/index.tsx
@@ -1,4 +1,3 @@
-import { outfitFont } from "@/assets/fonts";
 import { simiosLogo } from "@/models/urlImgSwiper.model";
 import ParagraphSection from "@/components/sectionParagraph/sectionParagraph";
 
@@ -6,8 +5,7 @@ import clsx from "clsx";
 
 export const AboutSimios = () => {
   const Container = clsx(
-    "flex flex-col items-center mt-28 gap-6",
-    "max-[260px]:mt-20",
+    "flex flex-col items-center gap-6",
     `md:flex-row w-full`
   );
 
@@ -16,11 +14,7 @@ export const AboutSimios = () => {
     "max-[190px]:w-16 max-[225px]:w-24 max-[270px]:w-32"
   );
 
-  const paragraphAboutSimios = clsx(
-    `${outfitFont.className} font-normal text-lg leading-6 text-center text-[#1b1b1bcc] max-w-80`,
-    "max-[160px]:text-[8px] max-[190px]:text-[10px] max-[225px]:text-xs max-[270px]:text-sm max-[305px]:text-base",
-    "md:w-full md:text-left md:max-w-[600px] md:text-[24px]"
-  );
+
 
   return (
     <section className={Container}>
@@ -29,11 +23,9 @@ export const AboutSimios = () => {
         src={simiosLogo.image}
         alt="Imagem do grupo Simios"
       />
-      {/* <p className={paragraphAboutSimios}>
-
-      </p> */}
+   
       <ParagraphSection border={false}>
-      Somos um coletivo dedicado ao desenvolvimento profissional, comprometido
+        Somos um coletivo dedicado ao desenvolvimento profissional, comprometido
         em aprimorar as habilidades dos colaboradores por meio de projetos
         voluntários abrangentes, com foco em temáticas sociais, ambientais e
         outros.
